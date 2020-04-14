a3a6fc0264dcbd4a19d72eb710e7bd49736e0d88
Amanda <a.thomson404@googlemail.com>
2020-04-14 14:38:56 +0100

    initial entity rec

--- scraper.py
+++ scraper.py
@@ -1,35 +1,48 @@
 import argparse
 import validators
 import requests
+import json
 from bs4 import BeautifulSoup
+import spacy
+nlp = spacy.load("en_core_web_md")
 
 
 def main(urls):
-    scraper(urls)
+    web = scraper(urls)
+    web_json = json.dumps(web)
+    extract_entities(web_json)
+
+
+def extract_entities(web_data):
+    web_json = json.loads(web_data)
+    for data in web_json:
+        stripped = data['body'].replace('"', '')
+        doc = nlp(stripped)
+        data['people'] = [ent for ent in doc.ents if ent.label_ == 'PERSON']
+        data['places'] = [ent for ent in doc.ents if ent.label_ == 'LOC']
+        data['organisations'] = [ent for ent in doc.ents if ent.label_ == 'ORG']
+
+        print(f"\nArticle Title: {data['title']}")
+        print(f"People identified: {data['people']}")
+        print(f"Places identified: {data['places']}")
+        print(f"Organisations identified: {data['organisations']}")
 
 
 def scraper(urls):
-    web_data = []
-    for url in urls:       
-        if(validators.url(url)):
+    web = []
+    for url in urls:
+        if validators.url(url):
+            web_site = {}
             r = requests.get(url)
             soup = BeautifulSoup(r.content, 'html.parser')
-            site_title = soup.title.string
-            dates = soup.find_all('div', attrs={'class': 'date date--v2'})    
-            site_date = dates[0]['data-datetime']
-            site_body = soup.find('div', attrs={'class': 'story-body__inner'}).get_text()    
-            web_data.append({ 'url': url, 'title': site_title, 'date':site_date, 'site_body': site_body })
+            web_site['title'] = soup.title.string
+            dates = soup.find_all('div', attrs={'class': 'date date--v2'})
+            web_site['date'] = dates[0]['data-datetime']
+            web_site['body'] = soup.find('div', attrs={'class': 'story-body__inner'}).get_text()
+            web.append(web_site)
 
-    print(web_data)        
-    
-def tag_visible(element):
-    print(element.child.name, 'elemet')
-    if element in ['style', 'script', 'head', 'title', 'meta', '[document]', 'li']:
-        return False
-    if isinstance(element, Comment):
-        return False
-    return True
-    
+    return web
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
